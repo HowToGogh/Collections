d119a24e1ee8d9e089f51d594da7471b230c2796
Ostap Kuchumov <ostapkuchumov@mail.ru>
2025-03-20 22:58:37 +0300

    Revert "main.c deletions commited, im tired so i going to drimk some tea with cookies )))"
    This reverts commit d1f5254d866dae712db05c9a163122e0ae2ad838.

---
+++ TODO.c
@@ -0,0 +1,21 @@
+/*
+
+
+    TODO:
+*   clear comments
+*   add static
+*   dynamic size
+*   try circular buffer
+
+
+
+!   FIXME:
+!
+!   add checks of double-free and double-alloc
+!   free ptr on Stack and Queue
+!   break the function if error
+!   add FAILURE_CODE if needed
+
+
+
+*/
